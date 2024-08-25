8f0d84b6f15fa84a64c9ce3d0d32673a4fe18b77
abhi-crewhub <abhishek@crewhub.in>
2024-08-26 00:26:59 +0530

    repository method to apisevice added with error handling

--- app/src/main/java/com/abhi/atlysmovieapp/repository/MovieRepository.kt
+++ app/src/main/java/com/abhi/atlysmovieapp/repository/MovieRepository.kt
@@ -1,6 +1,26 @@
 package com.abhi.atlysmovieapp.repository
 
+import android.util.Log
+import com.abhi.atlysmovieapp.model.Movie
+import com.abhi.atlysmovieapp.util.dummyMovies
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
 class MovieRepository {
 
+    fun getMovies(): Flow<Result<List<Movie>>> = flow {
 
+        try {
+            val response = movieApiService.getMovies()
+            if (response.isSuccessful) {
+                val movies = response.body() ?: emptyList()
+                emit(Result.success(movies))
+            } else {
+                emit(Result.failure(Exception("Error ${response.code()}: ${response.message()}")))
+            }
+        } catch (e: Exception) {
+            Log.d("TAG", "getMovies: ${e}")
+            emit(Result.failure(e))
+        }
+    }
 }
